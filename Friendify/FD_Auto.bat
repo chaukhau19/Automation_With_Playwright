875755f4c4c785d563deb5b30e1ed49f683d9ed5
chaukhau19 <chaukhau2000@gmail.com>
2024-09-08 13:01:52 +0700

    commit FD

--- Friendify/FD_Auto.bat
+++ Friendify/FD_Auto.bat
@@ -1,4 +1,5 @@
 @REM Friendify AI
+cd /d D:\Git\Automation_With_Playwright\Friendify
 
 call npx playwright test FD_Register.spec.js --headed --reporter=line --workers=1
 call npx playwright test FD_Login.spec.js --headed --reporter=line --workers=1
