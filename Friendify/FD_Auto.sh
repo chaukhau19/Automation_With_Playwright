f09521f91c5eb14109c4b20567005975a0b0a7b2
chaukhau19 <chaukhau2000@gmail.com>
2024-09-09 10:14:15 +0700

    update library

---
+++ Friendify/FD_Auto.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Friendify AI
+
+# Navigate to the directory where the tests are located
+# cd /d D:/Git/Automation_With_Playwright/Friendify
+
+# Run the Playwright tests
+npx playwright test FD_Register.spec.js --headed --reporter=line --workers=1
+npx playwright test FD_Login.spec.js --headed --reporter=line --workers=1
+npx playwright test FD_Actions.spec.js --headed --reporter=line --workers=1
