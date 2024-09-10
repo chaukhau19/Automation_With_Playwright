254b6c9ee4613b639b2e1163fa87a42de3cb3921
chaukhau19 <chaukhau2000@gmail.com>
2024-09-10 19:03:55 +0700

    FD

--- Friendify/FD_Auto.bat
+++ Friendify/FD_Auto.bat
@@ -2,5 +2,5 @@
 @REM cd /d D:\Git\Automation_With_Playwright\Friendify
 
 call npx playwright test FD_Register.spec.js --reporter=line --workers=1
-@REM call npx playwright test FD_Login.spec.js --reporter=line --workers=1
-@REM call npx playwright test FD_Actions.spec.js --reporter=line --workers=1
+call npx playwright test FD_Login.spec.js --reporter=line --workers=1
+call npx playwright test FD_Actions.spec.js --reporter=line --workers=1
