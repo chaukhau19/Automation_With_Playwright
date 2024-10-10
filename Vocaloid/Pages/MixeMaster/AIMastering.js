bcb0003626d3615a7d2347864f789b5ce5f3989b
chaukhau19 <chaukhau2000@gmail.com>
2024-10-10 14:14:47 +0700

    update cicd

--- Vocaloid/Pages/MixeMaster/AIMastering.js
+++ Vocaloid/Pages/MixeMaster/AIMastering.js
@@ -4,26 +4,24 @@ class AIMasteringPage {
   constructor(page) {
     this.page = page;
   }
-
   async AIMastering() {
     try {
       await this.page.getByRole('link', { name: 'AI Mastering' }).click();
 
-      await this.page.getByText('or Drop a File here...').first().click();
-      const filePath1 = config.Music_Audio_1;
-      await this.page.setInputFiles('input[type="file"]', filePath1);
-      console.log('File uploaded');
+      const filePath1 = 'D:/Git/Automation_With_Playwright/Vocaloid/Data/Ca khúc Cám ơn tình yêu.mp3';
+      await this.page.setInputFiles("(//input[@type='file'])", filePath1); 
+      console.log('First file uploaded');
 
-      await this.page.getByText('or Drop a File here...').nth(1).click();
-      const filePath2 = config.Music_Audio_3;
-      await this.page.setInputFiles('input[type="file"]', filePath2);
-      console.log('File uploaded');
+      // await this.page.locator("(//div[@class='sc-hnwOTO gDNkLc']//label[@id='file-upload'])[2]").click();
+      // const filePath2 = 'D:/Git/Automation_With_Playwright/Vocaloid/Data/Another_File.mp3';
+      // await this.page.setInputFiles("(//input[@type='file'])", filePath2); 
+      // console.log('Second file uploaded');
 
       await this.page.getByRole('button', { name: 'Start Mastering' }).click();
 
       const validLinkMessage = await this.page.getByText('Generate mastering');
       await expect(validLinkMessage).toBeVisible();
-
+  
       if (await validLinkMessage.isVisible()) {
         console.log('Generate mastering');
       }
@@ -31,7 +29,7 @@ class AIMasteringPage {
       console.error('Error during the Create Lyrics process:', error);
       throw new Error('Failed in the Create Lyrics process due to an error.');
     }
-  }
+  }  
 }
 
 export default AIMasteringPage;
