61203ae14c919c15a3779bdb6c4bb19957787c91
chaukhau19 <chaukhau2000@gmail.com>
2024-10-15 14:20:24 +0700

    y

--- Tongram/TG_Auto.sh
+++ Tongram/TG_Auto.sh
@@ -2,7 +2,8 @@
 
 # npx playwright test TG_Cookies.spec.js --reporter=line --workers=1
 # npx playwright test TG_DailyTask.spec.js --reporter=line --workers=1
-npx playwright test TG_VerifyPoint.spec.js --reporter=line --workers=1
+npx playwright test TG_VerifyPoint.spec.js --reporter=html,json --output=./Results --workers=1
+
 # npx playwright test TG_Actions.spec.js --reporter=line --workers=1
 # npx playwright test TG_Languages.spec.js --reporter=line --workers=1
 
