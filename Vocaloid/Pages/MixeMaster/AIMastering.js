730832e3502ff9cd7352852b12a1e2fed95c095b
chaukhau19 <chaukhau2000@gmail.com>
2024-10-15 10:53:44 +0700

    test clear log

--- Vocaloid/Pages/MixeMaster/AIMastering.js
+++ Vocaloid/Pages/MixeMaster/AIMastering.js
@@ -5,47 +5,43 @@ class AIMasteringPage {
   constructor(page) {
     this.page = page;
   }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////
   async uploadFile(filePath) {
     await this.page.setInputFiles("(//input[@type='file'])", filePath);
     console.log(`✅ File uploaded: ${filePath}`);
   }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////
   logTimeTaken(startTime) {
     const timeTaken = Math.round((Date.now() - startTime) / 1000);
     const minutes = Math.floor(timeTaken / 60);
     const seconds = timeTaken % 60;
     console.log(`🕒 Time taken: ${minutes} minutes and ${seconds} seconds`);
   }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////
   async AIMastering() {
     try {
-      // Navigate to AI Mastering page and upload file
       await this.page.getByRole('link', { name: 'AI Mastering' }).click();
       const filePath1 = config.Music_Audio_1;
       await this.uploadFile(filePath1);
       
-      // Start mastering process
       await this.page.getByRole('button', { name: 'Start Mastering' }).click();
-      await expect(this.page.getByText('Generate mastering')).toBeVisible();
+      // await expect(this.page.getByText('Generate mastering')).toBeVisible();
+      const successGenerate = this.page.getByText('Generate mastering');
+      await successGenerate.waitFor({ state: 'visible', timeout: 100000 });      
       console.log('✅ Generate mastering visible');
 
-      // Wait for header to be visible
       const headerLocator = this.page.locator(`//div[@class='header']//h2[text()='${config.Music_Text_1}']`);
       const startTime = Date.now();
       await expect(headerLocator).toBeVisible({ timeout: 100000 });
 
-      // Wait for success message
       const successMessageLocator = this.page.locator(`//span[div[contains(text(), 'Success')]]`);
       await successMessageLocator.waitFor({ state: 'visible', timeout: 400000 });
       this.logTimeTaken(startTime);
 
-      // Navigate to History Activities
       await this.page.getByRole('link', { name: 'History Activities' }).click();
       await this.page.waitForSelector(`//h6[text()='${config.Music_Text_1}']`, { state: 'visible' });
       await this.page.waitForSelector(`(//td[.//div[text()='AI Mastering'] and .//h6[text()='${config.Music_Text_1}']])[1]`, { state: 'visible' });
 
-      // Check for success or failure in history
       const successTd = this.page.locator(`//tr[.//h6[text()='${config.Music_Text_1}']][1]//p[text()='Success']`);
       const failedTd = this.page.locator(`//tr[.//h6[text()='${config.Music_Text_1}']][1]//p[text()='Failed']`);
       
@@ -60,7 +56,6 @@ class AIMasteringPage {
         console.log('❌ Test status unknown: No success or failure message found.');
       }
 
-      // Delete the song from history
       await this.page.waitForSelector(`//tr[.//h6[text()='${config.Music_Text_1}']][1]//button[contains(@class, 'delete-button')]`, { state: 'visible' });
       await this.page.locator(`//tr[.//h6[text()='${config.Music_Text_1}']][1]//button[contains(@class, 'delete-button')]`).click();
 
@@ -73,6 +68,6 @@ class AIMasteringPage {
       throw new Error('❌ Failed in the AI Mastering process: ' + error.message);
     }
   }
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 }
-
 export default AIMasteringPage;
