3082fdbcf774468bc94c235d8209c20d5fc24ef3
chaukhau19 <chaukhau2000@gmail.com>
2024-10-21 15:58:43 +0700

    update cookies_tongram

--- Tongram/TG_Auto.bat
+++ Tongram/TG_Auto.bat
@@ -6,4 +6,6 @@
 @REM call npx playwright test TG_Actions.spec.js --headed --reporter=line --workers=1
 @REM call npx playwright test TG_Languages.spec.js --headed --reporter=line --workers=1
 
-call npx playwright test TG_DailyTask.spec.js TG_VerifyPoint.spec.js TG_Actions.spec.js TG_Languages.spec.js --reporter=html --output=./Results --workers=1
+@REM call npx playwright test TG_DailyTask.spec.js TG_VerifyPoint.spec.js TG_Actions.spec.js TG_Languages.spec.js --reporter=html --output=./Results --workers=1 --headed
+
+call npx playwright test TG_DailyTask.spec.js TG_VerifyPoint.spec.js TG_Actions.spec.js TG_Languages.spec.js --reporter=html --output=./Results --workers=1 
