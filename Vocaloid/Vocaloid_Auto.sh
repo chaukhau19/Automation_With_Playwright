e8823d2efe9a82189a5941641c4c0618047b3323
chaukhau19 <chaukhau2000@gmail.com>
2024-11-14 11:33:39 +0700

    update cookie

--- Vocaloid/Vocaloid_Auto.sh
+++ Vocaloid/Vocaloid_Auto.sh
@@ -11,4 +11,4 @@
 # npx playwright test History.spec.js --headed --reporter=line --workers=1
 # npx playwright test ConvertVoice.spec.js CreateLyrics.spec.js CreateMusic.spec.js CreateMelody.spec.js AIMastering.spec.js MusicSplitter.spec.js RemoveVocal.spec.js AvatarMV.spec.js CreateMVMusic.spec.js History.spec.js --headed --reporter=html --output=./Results --workers=1
 
-npx playwright test ConvertVoice.spec.js CreateLyrics.spec.js CreateMusic.spec.js CreateMelody.spec.js AIMastering.spec.js MusicSplitter.spec.js RemoveVocal.spec.js AvatarMV.spec.js CreateMVMusic.spec.js History.spec.js --reporter=html --output=./Results --workers=1
+npx playwright test ConvertVoice.spec.js CreateLyrics.spec.js CreateMusic.spec.js CreateMelody.spec.js MusicSplitter.spec.js RemoveVocal.spec.js AvatarMV.spec.js CreateMVMusic.spec.js History.spec.js --reporter=html --output=./Results --workers=1
